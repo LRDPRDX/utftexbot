f32019dcb5e173da556a8ce9adb761cd790e495f
LRDPRDX <paradox1859@gmail.com>
2025-11-29 06:09:46 +0700

    Add message limit

--- botex/logic.py
+++ botex/logic.py
@@ -13,8 +13,15 @@ def nothrow(func) :
             logger.error(f'Exception raised from the logic module:\n{e}')
     return wrapper
 
-def latex2string(latex: str) -> tuple[bool, str] :
+def latex2string(latex: str, limit: int | None = None) -> tuple[bool, str] :
     timeout = config['process'].getfloat('timeout')
+
+    if not limit:
+        limit = config['message'].getint('length')
+
+    if len(latex) > limit :
+        return False, f'Expression too long (maximum length: {limit} characters)'
+
     try :
         p = subprocess.run(['/usr/local/bin/utftex', latex],
                            capture_output = True,
